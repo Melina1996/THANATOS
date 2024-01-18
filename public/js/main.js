a3628344401be370ba1567e37ab1616128f92143
Melina1996 <melina.schlachter@gmx.de>
2024-01-18 15:59:54 +0100

    added first try JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,41 @@
+//variables for each of my h2
 
-let myDiv = document.querySelector(".hello")
+let firstH2 = document.querySelector("#firstH2")
 
-function changeColor(){
-  myDiv.classList.add("ciao")
+let secondH2 = document.querySelector("#secondH2")
+
+let thirdH2 = document.querySelector("#thirdH2")
+
+//LOOP for numbers
+
+//counter twitter
+
+let myNumber
+
+
+  for (let i = 0; i < 12000; i++) {
+    setTimeout(function(){
+      firstH2.innerText = i
+    }, 1000)
+  }
+
+
+//counter youtube
+
+function counterTwo(){
+  for (let i = 0; i <= 5000; i++) {
+    setTimeout(secondH2.innerText = i++,2000) 
+  }
 }
 
-myDiv.addEventListener("click",changeColor)
+
+//counter youtube
+
+function counterThree(){
+  for (let i = 0; i <= 7500; i++) {
+    setTimeout(thirdH2.innerText = i++,2000) 
+  }
+}
+
+
 
