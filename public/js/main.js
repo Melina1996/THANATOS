215ee22303ed01602a35230f3d9ec72d54e001b7
Melina1996 <melina.schlachter@gmx.de>
2024-01-20 14:40:24 +0100

    changed CSS and added comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,41 @@
-//variables for each of my h2
+//declare variables 
 
-let allSocialMedia = document.querySelectorAll(".each-logo span")
+//all my three counters:
+let allCounters = document.querySelectorAll(".each-logo span")
 
-allSocialMedia.forEach(element => {
+//to adjust style of my logos and my paragraphs in the end
+let myContainer = document.querySelectorAll(".fa-brands")
+let myPs = document.querySelectorAll("p")
+
+//FOR EACH LOOP for my 3 counters
+allCounters.forEach(element => {
   element.innerText = "0";
 
+//for each counter I call the function here:
   function counting(){
+    //target number is defined in the attribute "data-target" of each counter
     let maxCount = +element.getAttribute("data-target")
     let myH2Count = +element.innerText
 
     let increment = maxCount/200 //will increment by 200er-steps
 
-    if(myH2Count < maxCount){
-      element.innerText = Math.round(myH2Count + increment);
-      setTimeout(counting,1)
-    } else {
-      element.innerText = maxCount
+      //if number of my counter smaller than target, my inner Text is equal to the counter
+      if(myH2Count < maxCount){
+        element.innerText = Math.round(myH2Count + increment);
+        setTimeout(counting,1) //time span is 1second, call function here as long as my number smaller than target
+      } else { //when target reached, inner Text is assigned the target value
+        element.innerText = maxCount
+        //change styles of my paragraph and logos
+        element.style.color = "black"
+        myIcons.forEach(element => {
+          element.style.color = "black"
+        });
+        myPs.forEach(element => {
+          element.style.color = "black"
+        });
+      }
     }
-  }
-  counting()
+    counting()
   })
 
 
